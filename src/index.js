f47876e896e2144ca66e4e1f7c9d3049bee84ad8
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2023-05-24 17:38:33 +0300

    add express config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 
+
+const expressConfig = require('./config/expressConfig');
+const handlebarsConfig = require('./config/handlebarsConfig');
+
 const app = express();
 
 const PORT = 5000;
 
+expressConfig(app);
+handlebarsConfig(app);
+
+
+
+
+// Routes
 app.get('/', (req, res) => {
-    res.send('Hello from express')
+    res.render('index')
 })
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`))
