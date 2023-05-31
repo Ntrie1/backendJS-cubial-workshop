33db94854eb8d1def20424f38615a31b84f70fc7
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2023-05-31 14:06:06 +0300

    show attached accessories

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -17,13 +17,14 @@ router.post('/create', async (req, res) =>{
 });
 
 router.get('/:cubeId/details', async (req, res) =>{
-    const cube = await cubeManager.getOne(req.params.cubeId).lean();
+    const cube = await cubeManager.getOneWithAccessories(req.params.cubeId).lean();
 
     if(!cube){
         return res.redirect('/404')
     }
 
-    res.render('details', { cube })
+    res.render('details', { cube
+     })
 })
 
 
