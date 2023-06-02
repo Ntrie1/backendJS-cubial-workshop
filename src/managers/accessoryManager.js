ccd1fe7fe070326ce91ada571d1c69ad6719ce31
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2023-06-02 20:43:27 +0300

    add remaining accessories

--- src/managers/accessoryManager.js
+++ src/managers/accessoryManager.js
@@ -2,4 +2,6 @@ const Accessory = require('../models/Accessory');
 
 exports.getAll = () => Accessory.find();
 
-exports.create = (accessoryData) => Accessory.create(accessoryData)
+exports.create = (accessoryData) => Accessory.create(accessoryData);
+
+exports.getOthers = (accessoryIds) => Accessory.find({ _id: { $nin: accessoryIds } }); 
